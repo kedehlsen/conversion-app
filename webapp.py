551dc6c216d6994adc6fc60e0efb8e66bbed13b2
kedehlsen <54326372+kedehlsen@users.noreply.github.com>
2019-10-11 09:34:15 -0700

    Create webapp.py

---
+++ webapp.py
@@ -0,0 +1,14 @@
+from flask import Flask, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def render_main():
+    return render_template('home.html')
+
+@app.route("/aboutus") #annotations tell which function goes with which request
+def render_page1():
+    return render_template('page1.html')
+    
+if __name__=="__main__":
+    app.run(debug=False)
