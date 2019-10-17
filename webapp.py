daae02d89462550a5fdb25170c6c31a94c6d3b04
kedehlsen <54326372+kedehlsen@users.noreply.github.com>
2019-10-17 09:45:27 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -9,6 +9,10 @@ def render_main():
 @app.route("/p1") #annotations tell which function goes with which request
 def render_page1():
     return render_template('page1.html')
+
+@app.route("/p2")
+def render_page2():
+    return render_template('page2.html')
     
 if __name__=="__main__":
     app.run(debug=False)
